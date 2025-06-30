e9d84efb5585f20ec9b0dc2e4df11dd42c0a833d
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-30 17:49:47 +0530

    we set json recieved json

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,14 +2,13 @@
 
 use poem::{
     Route, Server, get, handler,
-    http::response,
     listener::TcpListener,
     post,
     web::{Json, Path},
 };
 
 use crate::{request_inputs::CreateWebsiteInput, request_output::CreateWebsiteOutput};
-
+use store::Store;
 pub mod request_inputs;
 pub mod request_output;
 
@@ -23,9 +22,10 @@ fn get_website(Path(website_id): Path<String>) -> String {
 fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
     let url = data.url;
     // persist this is a db
-    let response = CreateWebsiteOutput {
-        id: String::from("ID"),
-    };
+    // calling and using the store
+    let s = Store {};
+    let id = s.create_website();
+    let response = CreateWebsiteOutput { id };
     Json(response)
 }
 
