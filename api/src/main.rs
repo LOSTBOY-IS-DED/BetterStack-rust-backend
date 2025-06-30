48f3353b24a3ffb993a6217154bd8f8362810059
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-30 17:05:33 +0530

    made three endpoint learnt bout query param here

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,16 @@
 // use std::net::TcpListener; -> you can create a http listener on top of this
 
-use poem::{IntoResponse, Route, Server, get, handler, listener::TcpListener, web::Path};
+use poem::{Route, Server, get, handler, listener::TcpListener, post, web::Path};
+
+#[handler]
+fn get_website(Path(website_id): Path<String>) -> String {
+    format!("website: {}", website_id)
+}
+
+#[handler]
+fn create_website(Path(website_id): Path<String>) -> String {
+    format!("website: {}", website_id)
+}
 
 #[handler]
 fn hello(Path(name): Path<String>) -> String {
@@ -9,7 +19,12 @@ fn hello(Path(name): Path<String>) -> String {
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Route::new().at("/hello/:name", get(hello));
+    // specify the business details of your app
+    let app = Route::new()
+        .at("/hello/:name", get(hello))
+        .at("/status/:website_id", get(create_website))
+        .at("/website", post(create_website));
+    // creates and run the http server
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
         .await
